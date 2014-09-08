24f2c3ea1f6943c008ca7baffb6eb8dbe3baed50
michballard <michballard@gmail.com>
2014-09-08 17:25:33 +0100

    Created users, session id, bcrypt

--- bookmark-manager/server.rb
+++ bookmark-manager/server.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 require 'data_mapper'
 
+enable :sessions
+set :session_secret, 'super secret'
+
 env = ENV["RACK_ENV"] || "development"
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
 require './lib/tag'
+require './lib/user'
 
 DataMapper.finalize
 
@@ -31,4 +35,22 @@ get '/tags/:text' do
 	tag = Tag.first(:text => params[:text])
 	@links = tag ? tag.links : []
 	erb :index
+end
+
+get '/users/new' do
+	erb :"users/new"
+end
+
+post '/users' do
+	user = User.create(:email => params[:email],
+							:password => params[:password])
+	session[:user_id] = user.id
+	redirect to('/')
+end
+
+helpers do
+	def current_user
+		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+	end
+
 end
