0ebb6fee5891afff21f6953f00bc8a32a2c7325f
michballard <michballard@gmail.com>
2014-09-08 12:32:29 +0100

    Started bookmark manager tasks - new link added

---
+++ bookmark-manager/server.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'data_mapper'
+
+env = ENV["RACK_ENV"] || "development"
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
+
+get '/' do
+@links = Link.all 
+	erb :index
+end
+
+post '/links' do 
+	url = params["url"]
+	title = params["title"]
+	Link.create(:url => url, :title => title)
+	redirect to('/')
+end
