537f2a158679adde6034d9efc2bbeb841e4b9df7
michballard <michballard@gmail.com>
2014-09-11 21:32:39 +0100

    Separated index view from links adding

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -5,7 +5,9 @@ feature "User adds a new link" do
 	scenario "when browsing the homepage" do 
 		expect(Link.count).to eq(0)
 		visit '/'
+		visit '/links/new'
 		add_link("http://www.makersacademy.com/", "Makers Academy")
+		visit '/'
 		expect(Link.count).to eq(1)
 		link = Link.first
 		expect(link.url).to eq("http://www.makersacademy.com/")
@@ -13,10 +15,11 @@ feature "User adds a new link" do
 	end
 
 	scenario "with a few tags" do 
-		visit "/"
+		visit "/links/new"
 		add_link("http://www.makersacademy.com/", 
 									"Makers Academy", 
 									['education', 'ruby'])
+		visit '/'
 		link = Link.first
 		expect(link.tags.map(&:text)).to include("education")
 		expect(link.tags.map(&:text)).to include("ruby")
